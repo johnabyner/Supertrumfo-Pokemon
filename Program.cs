7f5ba077b300f6d5c68e65b5fb33e2c575497805
johnabyner <abynergames>
2026-04-08 16:15:44 -0300

    feat: changed array for list, for a clean code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using supertrumfo.dominio;
 
+using System.Collections.Generic;
+
 public class Program
 {
     private static Random rng = new Random();
@@ -15,72 +17,40 @@ public class Program
         //Pontos
         int[] points = new int[2];
 
-        //Pokemons
-        Pokemon pikachu = new Pokemon("Pikachu", PokemonType.Eletrico, 55, 40, 90, 50, 50);
-        Pokemon greninja = new Pokemon("Greninja", PokemonType.Agua, 95, 67, 122, 103, 71);
-        Pokemon swampert = new Pokemon("Swampert", PokemonType.Agua, 110, 90, 60, 85, 90);
-        Pokemon talonflame = new Pokemon("Talonflame", PokemonType.Fogo, 81, 71, 126, 74, 69);
-        Pokemon charizard = new Pokemon("Charizard", PokemonType.Fogo, 84, 78, 100, 109, 85);
-        Pokemon torterra = new Pokemon("Torterra", PokemonType.Grama, 109, 105, 56, 75, 85);
-        Pokemon sceptile = new Pokemon("Sceptile", PokemonType.Grama, 85, 65, 120, 105, 85);
-        Pokemon magnemite = new Pokemon("Magnemite", PokemonType.Eletrico, 35, 70, 45, 95, 55);
-        Pokemon luxray = new Pokemon("Luxray", PokemonType.Eletrico, 120, 79, 70, 95, 79);
-        Pokemon garchomp = new Pokemon("Garchomp", PokemonType.Rocha, 130, 95, 102, 80, 85);
-        Pokemon krookodile = new Pokemon("Krookodile", PokemonType.Rocha, 117, 80, 92, 65, 70);
-        Pokemon vaporeon = new Pokemon("Vaporeon", PokemonType.Agua, 65, 60, 65, 110, 95);
-        Pokemon gyarados = new Pokemon("Gyarados", PokemonType.Agua, 125, 79, 81, 60, 100);
-        Pokemon lapras = new Pokemon("Lapras", PokemonType.Agua, 85, 80, 60, 85, 95);
-        Pokemon starmie = new Pokemon("Starmie", PokemonType.Agua, 75, 85, 115, 100, 85);
-        Pokemon arcanine = new Pokemon("Arcanine", PokemonType.Fogo, 110, 80, 95, 100, 80);
-        Pokemon magmar = new Pokemon("Magmar", PokemonType.Fogo, 95, 57, 93, 100, 85);
-        Pokemon flareon = new Pokemon("Flareon", PokemonType.Fogo, 130, 60, 65, 95, 110);
-        Pokemon houndoom = new Pokemon("Houndoom", PokemonType.Fogo, 90, 50, 95, 110, 80);
-        Pokemon meganium = new Pokemon("Meganium", PokemonType.Grama, 82, 100, 80, 83, 100);
-        Pokemon bellossom = new Pokemon("Bellossom", PokemonType.Grama, 80, 95, 50, 90, 100);
-        Pokemon roserade = new Pokemon("Roserade", PokemonType.Grama, 70, 65, 90, 125, 105);
-        Pokemon tropius = new Pokemon("Tropius", PokemonType.Grama, 68, 83, 51, 72, 87);
-        Pokemon jolteon = new Pokemon("Jolteon", PokemonType.Eletrico, 65, 60, 130, 110, 95);
-        Pokemon electabuzz = new Pokemon("Electabuzz", PokemonType.Eletrico, 83, 57, 105, 95, 85);
-        Pokemon ampharos = new Pokemon("Ampharos", PokemonType.Eletrico, 75, 85, 55, 115, 90);
-        Pokemon manectric = new Pokemon("Manectric", PokemonType.Eletrico, 75, 60, 105, 105, 60);
-        Pokemon donphan = new Pokemon("Donphan", PokemonType.Rocha, 120, 120, 50, 60, 60);
-        Pokemon hippowdon = new Pokemon("Hippowdon", PokemonType.Rocha, 112, 118, 47, 68, 72);
-        Pokemon flygon = new Pokemon("Flygon", PokemonType.Rocha, 100, 80, 100, 80, 80);
-        Pokemon nidoking = new Pokemon("Nidoking", PokemonType.Rocha, 102, 77, 85, 85, 75);
         //Cartas
-        Pokemon[] cards =
+        List<Pokemon> cards = new List<Pokemon>
         {
-            pikachu,
-            greninja,
-            swampert,
-            talonflame,
-            charizard,
-            torterra,
-            sceptile,
-            magnemite,
-            luxray,
-            garchomp,
-            krookodile,
-            vaporeon,
-            gyarados,
-            lapras,
-            starmie,
-            arcanine,
-            magmar,
-            flareon,
-            houndoom,
-            meganium,
-            bellossom,
-            roserade,
-            tropius,
-            jolteon,
-            electabuzz,
-            ampharos,
-            manectric,
-            donphan,
-            hippowdon,
-            flygon,
-            nidoking
+            new Pokemon("Pikachu", PokemonType.Eletrico, 55, 40, 90, 50, 50),
+            new Pokemon("Greninja", PokemonType.Agua, 95, 67, 122, 103, 71),
+            new Pokemon("Swampert", PokemonType.Agua, 110, 90, 60, 85, 90),
+            new Pokemon("Talonflame", PokemonType.Fogo, 81, 71, 126, 74, 69),
+            new Pokemon("Charizard", PokemonType.Fogo, 84, 78, 100, 109, 85),
+            new Pokemon("Torterra", PokemonType.Grama, 109, 105, 56, 75, 85),
+            new Pokemon("Sceptile", PokemonType.Grama, 85, 65, 120, 105, 85),
+            new Pokemon("Magnemite", PokemonType.Eletrico, 35, 70, 45, 95, 55),
+            new Pokemon("Luxray", PokemonType.Eletrico, 120, 79, 70, 95, 79),
+            new Pokemon("Garchomp", PokemonType.Rocha, 130, 95, 102, 80, 85),
+            new Pokemon("Krookodile", PokemonType.Rocha, 117, 80, 92, 65, 70),
+            new Pokemon("Vaporeon", PokemonType.Agua, 65, 60, 65, 110, 95),
+            new Pokemon("Gyarados", PokemonType.Agua, 125, 79, 81, 60, 100),
+            new Pokemon("Lapras", PokemonType.Agua, 85, 80, 60, 85, 95),
+            new Pokemon("Starmie", PokemonType.Agua, 75, 85, 115, 100, 85),
+            new Pokemon("Arcanine", PokemonType.Fogo, 110, 80, 95, 100, 80),
+            new Pokemon("Magmar", PokemonType.Fogo, 95, 57, 93, 100, 85),
+            new Pokemon("Flareon", PokemonType.Fogo, 130, 60, 65, 95, 110),
+            new Pokemon("Houndoom", PokemonType.Fogo, 90, 50, 95, 110, 80),
+            new Pokemon("Meganium", PokemonType.Grama, 82, 100, 80, 83, 100),
+            new Pokemon("Bellossom", PokemonType.Grama, 80, 95, 50, 90, 100),
+            new Pokemon("Roserade", PokemonType.Grama, 70, 65, 90, 125, 105),
+            new Pokemon("Tropius", PokemonType.Grama, 68, 83, 51, 72, 87),
+            new Pokemon("Jolteon", PokemonType.Eletrico, 65, 60, 130, 110, 95),
+            new Pokemon("Electabuzz", PokemonType.Eletrico, 83, 57, 105, 95, 85),
+            new Pokemon("Ampharos", PokemonType.Eletrico, 75, 85, 55, 115, 90),
+            new Pokemon("Manectric", PokemonType.Eletrico, 75, 60, 105, 105, 60),
+            new Pokemon("Donphan", PokemonType.Rocha, 120, 120, 50, 60, 60),
+            new Pokemon("Hippowdon", PokemonType.Rocha, 112, 118, 47, 68, 72),
+            new Pokemon("Flygon", PokemonType.Rocha, 100, 80, 100, 80, 80),
+            new Pokemon("Nidoking", PokemonType.Rocha, 102, 77, 85, 85, 75),
         };
 
         //Na terceira rodada vai finalizar o jogo
@@ -123,8 +93,8 @@ public class Program
         //Randomize
         void randomCard()
         {
-            choices[0] = rng.Next(0, cards.Length);
-            choices[1] = rng.Next(0, cards.Length);
+            choices[0] = rng.Next(0, cards.Count);
+            choices[1] = rng.Next(0, cards.Count);
         }
         void showOptions(Pokemon userCard, Pokemon botCard)
         {
